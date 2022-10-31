fd62300a56a3a95718a417f5f542d7bcc9d40060
Sam <layzeesam@gmail.com>
2022-10-30 19:06:58 -0700

    test commit

--- group-assignment-03/stack_ptr.c
+++ group-assignment-03/stack_ptr.c
@@ -2,75 +2,111 @@
  * Stack containing race conditions
  */
 
-#include <stdio.h>   // printf
-#include <stdlib.h>  // malloc free exit
-#include <pthread.h> // pthread_create pthread_join
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <assert.h>
+
+pthread_mutex_t lock; // mutex lock for critical region
+// critical region is top
+// so to achieve maximum concurrency,
+// mutex lock should be set over smallest possible region
+// so it will be set in push and pop function
+// instead of in testStack
+
+// how it is correct
+// push -1
+// then run thread routing which pushs first and pops first
+// in indeterminate manner but after it finishes,
+// there should be same number -1
 
 // Linked list node
-typedef int value_t; // value type of the node (int)
-typedef struct Node  // node type
+typedef int value_t;
+typedef struct Node
 {
-    value_t data;      // data of the node (int)
-    struct Node *next; // pointer to the next node in the list (Node*)
-} StackNode;           // node type (Node)
+    value_t data;
+    struct Node *next;
+} StackNode;
 
 // Stack function declarations
-void push(value_t v, StackNode **top); // push a value to the stack (void)
-value_t pop(StackNode **top);          // pop a value from the stack (int)
-int is_empty(StackNode *top);          // check if the stack is empty (int)
+void push(value_t v, StackNode **top);
+value_t pop(StackNode **top);
+int is_empty(StackNode *top);
+// it will push 500*3 and pop 500*3 by each thread
+// therefore, total push: 500*3*200 and same pops
+void* testStack(void *arg){
+    // thread routing runs 500 times
+    StackNode *top = (StackNode *) arg;
+    
+    for(int i=0; i< 500; i++){
+        push(1, &top);
+        pop(&top);
+        push(2, &top);
+        pop(&top);
+        push(3, &top);
+        pop(&top);
+    }
+}
 
 int main(void)
 {
-    StackNode *top = NULL; // top of the stack (Node*) (NULL)
+    int nthreads = 200;
+    pthread_t threads[nthreads]; // creating arrays for threads
+    int thread_count =0;    
+    StackNode *top = NULL;
+    push(-1, &top); // it will checked after threads are done
+    printf("pushing first: -1\n");
+    // if last value is not -1, then it is not working properly
+    for(; thread_count < nthreads; thread_count++){
+        // let the threads run in weird manner concurrently
+        pthread_create(&threads[thread_count], NULL, testStack, top);
+    }
 
-    push(5, &top);  // push 5 to the stack (void)
-    push(10, &top); // push 10 to the stack (void)
-    pop(&top);      // pop a value from the stack (int)
-    push(15, &top); // push 15 to the stack (void)
-    pop(&top);      // pop a value from the stack (int)
-    pop(&top);      // pop a value from the stack (int)
-    push(20, &top); // push 20 to the stack (void)
+    // collect all threads here
+    for(int i = 0; i < nthreads; i++){
+        pthread_join(threads[i], NULL);
+    }
 
-    push(-5, &top);  // push -5 to the stack (void)
-    pop(&top);       // pop a value from the stack (int)
-    push(-10, &top); // push -10 to the stack (void)
-    pop(&top);       // pop a value from the stack (int)
-    pop(&top);       // pop a value from the stack (int)
-    push(-15, &top); // push -15 to the stack (void)
-    pop(&top);       // pop a value from the stack (int)
-    push(-20, &top); // push -20 to the stack (void)
+    // all threads have been collected
+    // now it is time to evaluate
+    // make sure stack is empty
+    
+    assert(pop(&top) == -1 && "Stack should be empty\n");
+    printf("After running 200 threads ...\n");
+    printf("passed: last pop is: -1\n");
 
-    return EXIT_SUCCESS; // return success
+    return 0;
 }
 
 // Stack function definitions
-void push(value_t v, StackNode **top) // push a value to the stack (void)
+void push(value_t v, StackNode **top)
 {
-    // allocate memory for a new node (Node*)
-    StackNode *new_node = malloc(sizeof(StackNode));
-
-    new_node->data = v;    // set the data of the new node (int)
-    new_node->next = *top; // set the next node of the new node (Node*)
-    *top = new_node;       // set the top of the stack (Node*)
+    StackNode * new_node = malloc(sizeof(StackNode));
+    new_node->data = v;
+    
+    pthread_mutex_lock(&lock);
+    //printf("pushing: %d\n", v);
+    new_node->next = *top;
+    *top = new_node;
+    pthread_mutex_unlock(&lock);
+    
 }
 
-value_t pop(StackNode **top) // pop a value from the stack (int)
+value_t pop(StackNode **top)
 {
-    if (is_empty(*top))    // check if the stack is empty (int)
-        return (value_t)0; // return 0
+    if (is_empty(*top)) return (value_t)0;
 
-    value_t data = (*top)->data; // get the data of the top node (int)
-    StackNode *temp = *top;      // get the top node (Node*)
-    *top = (*top)->next;         // set the top of the stack (Node*)
+    pthread_mutex_lock(&lock);    
+    value_t data  = (*top)->data;
+    //printf("poping: %d\n", data);
+    StackNode * temp  = *top;
+    *top = (*top)->next;    
+    free(temp);
+    pthread_mutex_unlock(&lock);
 
-    free(temp); // free the top node (void)
-
-    return data; // return the data of the top node (int)
+    return data;
 }
-int is_empty(StackNode *top) // check if the stack is empty (int)
-{
-    if (top == NULL) // check if the top is NULL (int)
-        return 1;    // return 1 (true) when the top is NULL
-    else
-        return 0; // return 0 (false) when the top is not NULL
+int is_empty(StackNode *top) {
+    if (top == NULL) return 1;
+    else return 0;
 }
